4173b0dbe5d0a2ad70edd217caf8c194926d538e
musaomondi <amokem@yahoo.com>
2021-10-21 12:14:31 +0300

    create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'register', to: 'devise/registrations#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
   root to: 'home#index'
 end

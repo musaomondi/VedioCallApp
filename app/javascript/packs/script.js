4d8c7da2d4ad6072e1ee69a363aa27ff3d2db6b8
musaomondi <amokem@yahoo.com>
2021-10-22 12:42:04 +0300

    add js to handle the scrolling effect

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -21,3 +21,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+/* Scroll to the developers section when the start button is clicked */
+const startBtn = document.getElementById('js-start-btn')
+const devSection = document.getElementById('js-developers')
+if (startBtn !== null) {
+  startBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    devSection.scrollIntoView({behavior: 'smooth', block: 'start'})
+  })
+}
